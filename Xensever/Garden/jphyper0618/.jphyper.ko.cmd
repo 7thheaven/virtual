1450d003574e26874f2f9b4cc6b10438312b08f0
Little7 <7thheaven177@gmail.com>
2014-06-18 13:03:54 +0800

    ns
    修复了环境，ddk换了新的，一些头文件又变了，简单处理了一下。

---
+++ Xensever/Garden/jphyper0618/.jphyper.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/test/jphyper/jphyper.ko := ld -r -m elf_i386 -T /usr/src/kernels/2.6.32.43-0.4.1.xs1.6.10.741.170752xen-i686/scripts/module-common.lds  -o /home/test/jphyper/jphyper.ko /home/test/jphyper/jphyper.o /home/test/jphyper/jphyper.mod.o
