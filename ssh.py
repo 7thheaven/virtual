cef411a44d88ac2d4ae49747f53fd855a6ab5acc
Little7 <7thheaven177@gmail.com>
2014-10-17 12:36:56 +0800

    Add UI
    simple UI，need to improve

--- ssh.py
+++ ssh.py
@@ -13,71 +13,140 @@ success=False
 
 paramiko.util.log_to_file('paramiko.log')
 s=paramiko.SSHClient()
-#s.load_system_host_keys()
-s.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-s.connect(hostname = hostname,username=username, password=password)
+
+def connectssh():
+    global s,hostname,username,password
+    #s.load_system_host_keys()
+    s.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    s.connect(hostname = hostname,username=username, password=password)
 
 def execshow(cmd):
-    global s
-    global stdin
-    global stdout
-    global stderr
-    global success
+    global s,stdin,stdout,stderr,success
     success=False
     print cmd
     stdin,stdout,stderr=s.exec_command(cmd)
-    if stderr.read():
-        print "stderr"
-        print stderr.read()
+    res=''
+    #read means take away,need to save
+    stderrstr=stderr.read()
+    stdoutstr=stdout.read()
+    if stderrstr:
+        res+='stderr\n'+stderrstr
+        print res
     else:
-        print "stdout"
-        print stdout.read()
+        #print "stdout\n"+stdoutstr
         success=True
+        res=stdoutstr
+    restext.SetValue(res)
 
 def cleanmesg():
-    global s
-    global stdin
-    global stdout
-    global stderr
-    global success
+    global s,stdin,stdout,stderr,success
     success=False
     stdin,stdout,stderr=s.exec_command('dmesg -c')
-    if stderr.read():
-        print "stderr"
-        print stderr.read()
+    stderrstr=stderr.read()
+    if stderrstr:
+        print "stderr\n"+stderrstr
     else:
         success=True
 
 def waitdone():
-    global s
-    global stdin
-    global stdout
-    global stderr
-    global success
+    global s,stdin,stdout,stderr,success
     success=False
     while not success:
         time.sleep(1)
         stdin,stdout,stderr=s.exec_command('cat /proc/jphyper/signal')
-        if stderr.read():
-            print "stderr"
-            print stderr.read()
+        stderrstr=stderr.read()
+        stdoutstr=stdout.read()
+        if stderrstr:
+            print "stderr\n"+stderrstr
         else:
-            print "stdout"
-            signal=stdout.read()
-            print signal
-            if signal == '0':
+            print stdoutstr
+            if stdoutstr == '0':
                 print "Fin."
                 success=True
                 break
 
-cleanmesg()
-execshow('insmod /home/jphyper/jphyper.ko')
-execshow('dmesg -c')
-execshow('/home/mvmfi 2 -1 17 0')
-execshow('dmesg -c')
-waitdone()
-execshow('dmesg -c')
-execshow('rmmod jphyper')
-execshow('dmesg -c')
-s.close()
-print 'All done.'
+def simpletest():
+    connectssh()
+    cleanmesg()
+    execshow('insmod /home/jphyper/jphyper.ko')
+    execshow('dmesg -c')
+    execshow('/home/mvmfi 2 -1 17 0')
+    execshow('dmesg -c')
+    waitdone()
+    execshow('dmesg -c')
+    execshow('rmmod jphyper')
+    execshow('dmesg -c')
+    s.close()
+    print 'All done.'
+
+def cancel(event):
+    s.close()
+
+def bye(event):
+    s.close()
+    face.Close()
+
+def connect(event):
+    global s,hostname,username,password
+    #s.load_system_host_keys()
+    hostname=iptext.GetValue()
+    username=unametext.GetValue()
+    password=passwtext.GetValue()
+    s.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    s.connect(hostname = hostname,username=username, password=password)
+    execshow('ls')
+
+sfi=wx.App()
+face=wx.Frame(None,title="Virtual Fault Inject Platform",size=(800,600))
+bkg=wx.Panel(face)
+#ip,username,password--UI
+iplabel=wx.StaticText(bkg,wx.NewId(),'IP :')
+unamelabel=wx.StaticText(bkg,wx.NewId(),'UserName :')
+passwlabel=wx.StaticText(bkg,wx.NewId(),'Password :')
+iptext=wx.TextCtrl(bkg)
+unametext=wx.TextCtrl(bkg)
+passwtext=wx.TextCtrl(bkg)
+iptext.SetValue('192.168.13.1')
+unametext.SetValue('root')
+passwtext.SetValue('secret')
+
+restext=wx.TextCtrl(bkg,style=wx.TE_MULTILINE|wx.HSCROLL)
+#switchfriend=wx.ListBox(bkg,-1,(0,0),(80,45),friendlist,wx.LB_SINGLE)
+
+#bottom button--UI
+conbutton=wx.Button(bkg,label='Connect')
+conbutton.Bind(wx.EVT_BUTTON,connect)
+cancelbutton=wx.Button(bkg,label='Cancel')
+cancelbutton.Bind(wx.EVT_BUTTON,cancel)
+exitbutton=wx.Button(bkg,label='Exit')
+exitbutton.Bind(wx.EVT_BUTTON,bye)
+
+#ip,username,password--layout
+hbox1=wx.BoxSizer()
+hbox2=wx.BoxSizer()
+hbox3=wx.BoxSizer()
+hbox1.Add(iplabel,proportion=1,flag=wx.EXPAND)
+hbox1.Add(iptext,proportion=2,flag=wx.EXPAND)
+hbox2.Add(unamelabel,proportion=1,flag=wx.EXPAND)
+hbox2.Add(unametext,proportion=2,flag=wx.EXPAND)
+hbox3.Add(passwlabel,proportion=1,flag=wx.EXPAND)
+hbox3.Add(passwtext,proportion=2,flag=wx.EXPAND)
+vbox1=wx.BoxSizer(wx.VERTICAL)
+vbox1.Add(hbox1,proportion=1,flag=wx.EXPAND|wx.ALL,border=5)
+vbox1.Add(hbox2,proportion=1,flag=wx.EXPAND|wx.ALL,border=5)
+vbox1.Add(hbox3,proportion=1,flag=wx.EXPAND|wx.ALL,border=5)
+
+#bottom button--layout
+hbox4=wx.BoxSizer()
+hbox4.Add(conbutton,proportion=1,flag=wx.EXPAND,border=5)
+hbox4.Add(cancelbutton,proportion=1,flag=wx.EXPAND,border=5)
+hbox4.Add(exitbutton,proportion=1,flag=wx.EXPAND,border=5)
+
+#All
+vbox=wx.BoxSizer(wx.VERTICAL)
+vbox.Add(vbox1,proportion=3,flag=wx.EXPAND|wx.ALL,border=5)
+vbox.Add(restext,proportion=6,flag=wx.EXPAND|wx.LEFT|wx.BOTTOM|wx.RIGHT,border=5)
+vbox.Add(hbox4,proportion=1,flag=wx.EXPAND)
+bkg.SetSizer(vbox)
+face.Show()
+sfi.MainLoop()
